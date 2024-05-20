05924ae40784537398d0a752bb35eb18fde65ce7
miDelino <163313071+miDelino@users.noreply.github.com>
2024-05-20 10:19:03 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,58 @@
-@extends('layouts.app')
+@extends('students.layout')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row mb-4">
+        <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Student Management System</h2>
             </div>
+            <div class="pull-right">
+            <a class="btn btn-success" href="{{ route('students.create') }}">Create New Student</a>
         </div>
     </div>
-</div>
+    </div>
+
+   
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    
+
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Gender</th>
+                <th>Address</th>
+                <th>Date of Birth:</th>
+                <th>Contact Numbers</th>
+                <th width="280px">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach ($students as $student)
+            <tr>
+                <td>Jandel</td>
+                <td>jandel@gmail.com</td>
+                <td>Male</td>
+                <td>Barangay Tanqui</td>
+                <td>August 14 2002</td>
+                <td>09983062238</td>
+                <td>
+                  
+
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+
 @endsection
+
+
+
+
